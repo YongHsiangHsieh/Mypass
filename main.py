2b516eed3fa288ac596c5653a1fa23003c0da4c4
Wilson <wilsonhsieh1216@gmail.com>
2023-10-22 15:50:19 +0100

    Complete password manager project

--- main.py
+++ main.py
@@ -1,19 +1,54 @@
 from tkinter import *
+from tkinter import messagebox
+import random
+import pyperclip
 
 SMALL_PAD_Y_BOTTOM = (0, 3)
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    pass_text.delete(0, END)
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v',
+               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+               'R',
+               'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(2, 4)
+    nr_symbols = random.randint(1, 2)
+    nr_numbers = random.randint(2, 4)
+
+    password_list = ([random.choice(letters) for _ in range(nr_letters)] +
+                     [random.choice(symbols) for _ in range(nr_symbols)] +
+                     [random.choice(numbers) for _ in range(nr_numbers)])
+
+    random.shuffle(password_list)
+
+    password = ''.join(password_list)
+
+    pass_text.insert(0, password)
+    pyperclip.copy(password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_data():
-    with open('data.txt', mode='a') as data:
-        user = email_text.get()
-        website = web_text.get()
-        password = pass_text.get()
-        data.write(f"{user} => Website: {website}, Password: {password}\n")
-    web_text.delete(0, END)
-    pass_text.delete(0, END)
+    user = email_text.get().strip()
+    website = web_text.get().strip()
+    password = pass_text.get().strip()
+
+    if website == "" or password == "":
+        messagebox.showinfo(message="Don't leave any fields empty!")
+        return
+
+    is_ok = messagebox.askokcancel(title=website, message=f"Confirm {user} with password: {password}")
+    if is_ok:
+        with open('data.txt', mode='a') as data:
+            data.write(f"{user} => Website: {website}, Password: {password}\n")
+        web_text.delete(0, END)
+        pass_text.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -55,7 +90,7 @@ pass_label.grid(column=0, row=3, sticky='e')
 pass_text = Entry()
 pass_text.grid(column=1, row=3, sticky='nsew', pady=SMALL_PAD_Y_BOTTOM)
 # Generate Button
-gen_but = Button(text="Generate Password")
+gen_but = Button(text="Generate Password", command=generate_password)
 gen_but.grid(column=2, row=3, sticky='nsew', pady=SMALL_PAD_Y_BOTTOM)
 
 # Add
