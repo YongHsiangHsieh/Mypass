4b9b89cf4b962091eb8867e79eea781aa0dd6d43
Wilson <wilsonhsieh1216@gmail.com>
2023-10-22 11:40:17 +0100

    Add save_data() to save user's website and password to data.txt on click of add button

--- main.py
+++ main.py
@@ -2,9 +2,19 @@ from tkinter import *
 
 SMALL_PAD_Y_BOTTOM = (0, 3)
 
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save_data():
+    with open('data.txt', mode='a') as data:
+        user = email_text.get()
+        website = web_text.get()
+        password = pass_text.get()
+        data.write(f"{user} => Website: {website}, Password: {password}\n")
+    web_text.delete(0, END)
+    pass_text.delete(0, END)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -16,7 +26,7 @@ window.config(padx=50, pady=50)
 # logo
 logo = PhotoImage(file='logo.png')
 bg_logo = Canvas(width=200, height=200, highlightthickness=0)
-bg_logo.create_image(100, 100, image=logo)
+bg_logo.create_image(100, 100, anchor='center', image=logo)
 bg_logo.grid(column=1, row=0)
 
 # ----- Website -----
@@ -24,16 +34,18 @@ bg_logo.grid(column=1, row=0)
 web_label = Label(text="Website:")
 web_label.grid(column=0, row=1, sticky='e')
 # Text Entry
-web_text = Entry(width=35)
-web_text.grid(column=1, row=1, columnspan=2, sticky='w', pady=SMALL_PAD_Y_BOTTOM)
+web_text = Entry()
+web_text.grid(column=1, row=1, columnspan=2, sticky='nsew', pady=SMALL_PAD_Y_BOTTOM)
+web_text.focus()
 
 # ----- Email/Username -----
 # Label
 email_label = Label(text="Email/Username:")
 email_label.grid(column=0, row=2, sticky='e')
 # Email Entry
-email_text = Entry(width=35)
-email_text.grid(column=1, row=2, columnspan=2, sticky='w', pady=SMALL_PAD_Y_BOTTOM)
+email_text = Entry()
+email_text.grid(column=1, row=2, columnspan=2, sticky='nsew', pady=SMALL_PAD_Y_BOTTOM)
+email_text.insert(0, "Wilson@email.com")
 
 # ----- Password -----
 # Label
@@ -41,14 +53,14 @@ pass_label = Label(text="Password:")
 pass_label.grid(column=0, row=3, sticky='e')
 # Password Entry
 pass_text = Entry()
-pass_text.grid(column=1, row=3, sticky='w', pady=SMALL_PAD_Y_BOTTOM)
+pass_text.grid(column=1, row=3, sticky='nsew', pady=SMALL_PAD_Y_BOTTOM)
 # Generate Button
-gen_but = Button(text="Generate")
-gen_but.grid(column=2, row=3, sticky='w', pady=SMALL_PAD_Y_BOTTOM)
+gen_but = Button(text="Generate Password")
+gen_but.grid(column=2, row=3, sticky='nsew', pady=SMALL_PAD_Y_BOTTOM)
 
 # Add
 # Button
-add_but = Button(text="Add", width=36)
-add_but.grid(column=1, row=4, columnspan=2, sticky='w')
+add_but = Button(text="Add", command=save_data)
+add_but.grid(column=1, row=4, columnspan=2, sticky='nsew')
 
 window.mainloop()
